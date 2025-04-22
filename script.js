5c1113ebaf51eb3e9dced3be922ec6e8bd3c260d
Kacper Mazurczyk <kacper1609@wp.pl>
2025-04-22 20:58:31 +0200

    changed select tags with few options to ratio; introduced auto selection of country based on IP

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let clickCount = 0;
 
-const countryInput = document.getElementById('country');
-const myForm = document.getElementById('form');
-const modal = document.getElementById('form-feedback-modal');
-const clicksInfo = document.getElementById('click-count');
+const countryInput = document.getElementById("country");
+const myForm = document.getElementById("form");
+const modal = document.getElementById("form-feedback-modal");
+const clicksInfo = document.getElementById("click-count");
 
 function handleClick() {
     clickCount++;
@@ -12,27 +12,31 @@ function handleClick() {
 
 async function fetchAndFillCountries() {
     try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
+        const response = await fetch("https://restcountries.com/v3.1/all");
         if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
+            throw new Error("Błąd pobierania danych");
         }
         const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+        const countries = data.map((country) => country.name.common);
+        countryInput.innerHTML = countries
+            .sort()
+            .map((country) => `<option value="${country}">${country}</option>`)
+            .join("");
     } catch (error) {
-        console.error('Wystąpił błąd:', error);
+        console.error("Wystąpił błąd:", error);
     }
 }
 
 function getCountryByIP() {
-    fetch('https://get.geojs.io/v1/ip/geo.json')
-        .then(response => response.json())
-        .then(data => {
+    return fetch("https://get.geojs.io/v1/ip/geo.json")
+        .then((response) => response.json())
+        .then((data) => {
             const country = data.country;
             // TODO inject country to form and call getCountryCode(country) function
+            return country;
         })
-        .catch(error => {
-            console.error('Błąd pobierania danych z serwera GeoJS:', error);
+        .catch((error) => {
+            console.error("Błąd pobierania danych z serwera GeoJS:", error);
         });
 }
 
@@ -40,25 +44,39 @@ function getCountryCode(countryName) {
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        return response.json();
-    })
-    .then(data => {        
-        const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
-        // TODO inject countryCode to form
-    })
-    .catch(error => {
-        console.error('Wystąpił błąd:', error);
-    });
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Błąd pobierania danych");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const countryCode =
+                data[0].idd.root + data[0].idd.suffixes.join("");
+            // TODO inject countryCode to form
+
+            console.log(countryCode);
+        })
+        .catch((error) => {
+            console.error("Wystąpił błąd:", error);
+        });
 }
 
-
-(() => {
+(async () => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
-    document.addEventListener('click', handleClick);
+    document.addEventListener("click", handleClick);
 
-    fetchAndFillCountries();
-})()
+    const country = await getCountryByIP();
+
+    fetchAndFillCountries().then(() => {
+        const optionIndex = [...countryInput.options]
+            .map((option) => option.value)
+            .indexOf(country);
+
+        if (optionIndex >= 0) {
+            countryInput.value = country;
+        }
+    });
+
+    getCountryCode(country);
+})();
